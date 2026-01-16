6f1bb24ffc4cdc1f645fc40d6cbfb56d46c9a098
Viratmalakar <chandanmalakar730@gmail.com>
2026-01-16 14:16:44 +0530

    final app.py uploaded
    final app.py uploaded

--- app.py
+++ app.py
@@ -1,9 +1,11 @@
 from flask import Flask, render_template, request, redirect, session
 from users import USERS
+import os
 
 app = Flask(__name__)
 app.secret_key = "agent_secret_key"
 
+# ---------------- LOGIN ----------------
 @app.route("/", methods=["GET","POST"])
 def login():
     if request.method=="POST":
@@ -15,17 +17,20 @@ def login():
         return render_template("login.html", error="Invalid Username or Password")
     return render_template("login.html")
 
+# ---------------- DASHBOARD ----------------
 @app.route("/dashboard")
 def dashboard():
     if "user" not in session:
         return redirect("/")
     return render_template("index.html")
 
+# ---------------- LOGOUT ----------------
 @app.route("/logout")
 def logout():
     session.clear()
     return redirect("/")
 
+# ---------------- CHANGE PASSWORD ----------------
 @app.route("/change_password")
 def change_password():
     if session.get("user")!="admin":
@@ -45,10 +50,24 @@ def update_password():
     USERS["admin"] = new
     return render_template("login.html", success="Password changed successfully")
 
-import os
+# ---------------- UPLOAD PAGE ----------------
+@app.route("/upload")
+def upload():
+    if "user" not in session:
+        return redirect("/")
+    return render_template("upload.html")
 
+# ---------------- PROCESS EXCEL ----------------
+@app.route("/process", methods=["POST"])
+def process():
+    login = request.files['login_report']
+    cdr = request.files['cdr_report']
+    agent = request.files['agent_report']
+    crm = request.files['crm_report']
+
+    return "Files uploaded successfully"
+
+# ---------------- RUN SERVER ----------------
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
     app.run(host="0.0.0.0", port=port)
-
-
