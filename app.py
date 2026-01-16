cfe7836c3d402d57a9519635f082a0af5b21eb03
Viratmalakar <chandanmalakar730@gmail.com>
2026-01-16 15:12:39 +0530

    Modify process function for multiple file uploads
    Updated the process function to handle multiple file uploads.

--- app.py
+++ app.py
@@ -5,7 +5,6 @@ import os
 app = Flask(__name__)
 app.secret_key = "agent_secret_key"
 
-# ---------------- LOGIN ----------------
 @app.route("/", methods=["GET","POST"])
 def login():
     if request.method=="POST":
@@ -17,20 +16,17 @@ def login():
         return render_template("login.html", error="Invalid Username or Password")
     return render_template("login.html")
 
-# ---------------- DASHBOARD ----------------
 @app.route("/dashboard")
 def dashboard():
     if "user" not in session:
         return redirect("/")
     return render_template("index.html")
 
-# ---------------- LOGOUT ----------------
 @app.route("/logout")
 def logout():
     session.clear()
     return redirect("/")
 
-# ---------------- CHANGE PASSWORD ----------------
 @app.route("/change_password")
 def change_password():
     if session.get("user")!="admin":
@@ -39,8 +35,6 @@ def change_password():
 
 @app.route("/update_password", methods=["POST"])
 def update_password():
-    if session.get("user")!="admin":
-        return redirect("/")
     old = request.form["old_password"]
     new = request.form["new_password"]
 
@@ -50,24 +44,17 @@ def update_password():
     USERS["admin"] = new
     return render_template("login.html", success="Password changed successfully")
 
-# ---------------- UPLOAD PAGE ----------------
 @app.route("/upload")
 def upload():
     if "user" not in session:
         return redirect("/")
     return render_template("upload.html")
 
-# ---------------- PROCESS EXCEL ----------------
 @app.route("/process", methods=["POST"])
 def process():
-    login = request.files['login_report']
-    cdr = request.files['cdr_report']
-    agent = request.files['agent_report']
-    crm = request.files['crm_report']
+    files = request.files.getlist("files")
+    return f"{len(files)} files uploaded successfully"
 
-    return "Files uploaded successfully"
-
-# ---------------- RUN SERVER ----------------
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
     app.run(host="0.0.0.0", port=port)
