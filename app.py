5a4f9cdfc75a2d190c4c3034219a073b7662a329
Viratmalakar <chandanmalakar730@gmail.com>
2026-01-16 16:17:22 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,17 @@
-@app.route("/process", methods=["POST"])
+from flask import Flask, render_template, request, redirect, session
+import os
+
+app = Flask(__name__)
+app.secret_key = "testkey"
+
+@app.route("/")
+def home():
+    return "Server is running fine ✅"
+
+@app.route("/process", methods=["GET","POST"])
 def process():
-    return """
-    <h2>Server is running fine ✅</h2>
-    <p>Excel processing logic will be added step by step.</p>
-    <a href='/upload'>Go Back</a>
-    """
+    return "Process route working fine ✅"
+
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
