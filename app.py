e298a4e6535634c9cdc735d914be21c9f565547c
Viratmalakar <chandanmalakar730@gmail.com>
2026-02-05 13:18:32 +0530

    Update app.py

--- app.py
+++ app.py
@@ -108,7 +108,7 @@ def process():
 
     # ---- HIGHLIGHT FLAGS ----
     final["__red_net"]   = (login_sec > (8*3600 + 15*60)) & ((login_sec - break_sec) < 8*3600)
-    final["__red_break"] = break_sec > 40*60
+    final["__red_break"] = break_sec > 35*60
     final["__red_meet"]  = meet_sec  > 35*60
 
     # ---- GRAND TOTAL ----
@@ -171,3 +171,4 @@ def export():
     out.seek(0)
     return send_file(out, as_attachment=True,
                      download_name="Agent_Performance_Dashboard.xlsx")
+
